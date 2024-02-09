373b6c0785edefb8bdfdad4b5532263fd4046eb2
stephanielam0506 <129312642+stephanielam0506@users.noreply.github.com>
2024-02-09 00:25:38 -0800

    Add files via upload

--- birthdya/js/script.js
+++ birthdya/js/script.js
@@ -1,27 +1,7 @@
 'use strict';
 
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-// }
-
-const navItems = document.querySelectorAll('.nav a');
-
-// navItems.forEach(function() {
-
-navItems.addEventListener('hover', function() {
-    navItems.style.backgroundColor = '#ff0000';
-  });
-
-navItems.addEventListener('click', function(e) {
-    e.style.color = '#00ff00';
+$('.button').click(function() {
+  $('.button').css({
+    'background-color': 'red'
+  })
 });
-// });
-
-function change_page(){
-  window.location.href = "test.html";
-} 
